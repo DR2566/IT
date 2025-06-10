3009f40e07ebeca80f44fa0da5e0eb16a6fd6dce
DR2566 <rocek.david@email.cz>
2025-06-10 21:50:26 +0200

    studis_backups

--- pv004/studis.sh
+++ pv004/studis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATA_DIR="./data"
-BACK_DIR="./backup"
+BACKUP_DIR="./backup"
 
 FORCE=0
 
@@ -11,72 +11,124 @@ error_exit() {
     exit 1;
 }
 
-log_operation() {
-    # Získání aktuálního data a času v požadovaném formátu
-    timestamp=$(date +"%Y-%m-%d %H:%M.%S")
-
-    # Získání PID skriptu
-    pid=$$
-
-    # Získání přihlašovacího jména
-    login="$LOGNAME"
-    if [[ $login == "" ]]; then
-        login="x"
-    fi
-
-    # Zjištění způsobu spuštění skriptu (script name)
-    script_name=$(realpath "$0")
-
-    # Získání všech argumentů skriptu včetně voleb
-    args="$@"
-
-    ls "$DATA_DIR" 2>/dev/null >/dev/null
-    if [[ $? != 0 ]]; then
-        mkdir $DATA_DIR
-    fi
-    echo "$timestamp $pid $login $script_name $args" >> "${DATA_DIR}/log"
-}
-
 check_params_count() {
     if [[ $1 > $2 ]]; then
         error_exit "too much parameters"
     fi
 }
 
+#------------------ LOG UTILITIES ------------------
+
+check_data_dir() {
+    if [[ ! -d "$DATA_DIR" ]]; then
+        mkdir $DATA_DIR
+    fi
+}
+
+log_operation() {
+    timestamp=$(date +"%Y-%m-%d %H:%M.%S")
+
+    pid=$$
+
+    login="$LOGNAME"
+
+    if [[ $login == "" ]]; then
+        login="x"
+    fi
+
+    script_name=$(realpath "$0")
+
+    args="$@"
+
+    echo "$timestamp $pid $login $script_name $args" >> "${DATA_DIR}/log"
+}
+
+
 #------------------ FACULTIES UTILITIES ------------------
 
 fac_check_file() {
     ls "${DATA_DIR}/fak" 2>/dev/null >/dev/null
     if [[ $? != 0 ]]; then
-        touch "${DATA_DIR}/fak"
+        $(touch "${DATA_DIR}/fak")
     fi
 }
 
 fac_CREATE() {
+    fac_check_file
+
     id="$1"
     name="$2"
     # check id
-
+    if [[ "${#id}" > 8 ]]; then
+        error_exit "max id length is 8"
+    fi
     # check name
+    if [[ "$name" == "" ]]; then
+        error_exit "name is NONE"
+    fi
+    # check id in file
+    cat "${DATA_DIR}/fak" | grep "$id" >/dev/null 2>/dev/null
 
-    # create entity
-    echo
+    if [[ $? != 0 ]]; then
+        # create entity
+        echo "${id}|${name}" >> "${DATA_DIR}/fak"
+    else
+        error_exit "faculty id already exists"
+    fi
+
 }
 
 fac_READ() {
-    echo
+    fac_check_file
+    fac_id="$1"
+    # Záhlaví
+    printf "%-8s | %s\n" "Fakulta" "Název"
+    printf -- "--------------------------\n"
+
+    if [[ "$fac_id" == "" ]]; then
+        # Výpis, seřazený podle ID
+        sort "${DATA_DIR}/fak" | while IFS='|' read -r id name; do
+            printf "%-8s | %s\n" "$id" "$name"
+        done
+    else
+        row=$(cat "${DATA_DIR}/fak" | grep "$fac_id")
+        IFS='|' read -r id name <<< "$row"
+        printf "%-8s | %s\n" "$id" "$name"
+    fi
 }
 
 fac_UPDATE() {
-    echo
+    fac_check_file
+    id="$1"
+    name="$2"
 
+    cat "${DATA_DIR}/fak" | grep "$id" >/dev/null 2>/dev/null
+    if [[ "$?" == 0 ]]; then
+        fac_DELETE "$id"
+        fac_CREATE "$id" "$name"
+    else
+        error_exit "id does not exist"
+    fi
 }
 
 fac_DELETE() {
     fac_check_file
 
-    fac_id="$1"
-    sed -i "/${fac_id}/d" ${DATA_DIR}/fak
+    id="$1"
+
+    cat "${DATA_DIR}/fak" | grep "$id" >/dev/null 2>/dev/null
+    if [[ "$?" == 0 ]]; then
+        sed -i "/^${id}|/d" "${DATA_DIR}/fak"
+    else
+        error_exit "id does not exist"
+    fi
+}
+
+#------------------ BACKUPS UTILITIES ------------------
+check_backup_dir() {
+    if [[ ! -d "$BACKUP_DIR" ]]; then 
+        mkdir "$BACKUP_DIR"
+    fi
 }
 
 while getopts "d:b:f" opt_name; do 
@@ -85,7 +137,7 @@ while getopts "d:b:f" opt_name; do
             DATA_DIR="$OPTARG"
             ;;
         'b') 
-            BACK_DIR="$OPTARG"
+            BACKUP_DIR="$OPTARG"
             ;;
         'f')
             FORCE=1
@@ -96,6 +148,7 @@ while getopts "d:b:f" opt_name; do
     esac
 done
 
+check_data_dir
 args="$@"
 log_operation "$args"
 
@@ -159,7 +212,7 @@ case $1 in
         check_params_count $args_count 3
         fac_id="$2"
         fac_name="$3"
-        fac_CREATE $fac_id $fac_name
+        fac_CREATE "$fac_id" "$fac_name"
         ;;
     'fakulta-výpis'|'fakulta-vypis')
         check_params_count $args_count 2
@@ -170,12 +223,21 @@ case $1 in
         check_params_count $args_count 3
         fac_id="$2"
         fac_name="$3"
-        fac_UPDATE $fac_id $fac_name
+        fac_UPDATE "$fac_id" "$fac_name"
         ;;
     'fakulta-smaž'|'fakulta-smaz')
         check_params_count $args_count 2
         fac_id="$2"
+        if [[ "$fac_id" == "" ]]; then
+            error_exit "id not specified"
+        fi
         fac_DELETE "$fac_id"
+        ;;
+    #------------------ BACKUPS ------------------
+    'zálohy'|'zalohy')
+        check_params_count $args_count 1
+        check_backup_dir
+        ls -t "${BACKUP_DIR}"
         ;;
     *)
         echo 'bad command' 1>&2
